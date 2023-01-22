0e6a7a73e4feb6380ded8c70c70d393aff1d3898
Thunay <thunaymoreiradesoares@gmail.com>
2023-01-22 17:30:52 -0300

    Adiciona Tabs em Section Portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,61 @@
+/**
+ * jQuery One Page Scroll
+ */
 $(".main").onepage_scroll({
-    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-    easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-                                     // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-    animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-    pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-    beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-    afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-    loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-    keyboard: true,                  // You can activate the keyboard controls
-    responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-                                     // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-                                     // the browser's width is less than 600, the fallback will kick in.
-    direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
- });
+  sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+  easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+  // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+  animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+  pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+  updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+  beforeMove: function (index) { },  // This option accepts a callback function. The function will be called before the page moves.
+  afterMove: function (index) { },   // This option accepts a callback function. The function will be called after the page moves.
+  loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+  keyboard: true,                  // You can activate the keyboard controls
+  responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+  // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+  // the browser's width is less than 600, the fallback will kick in.
+  direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+});
 
- var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+/**
+ * Swiper
+ */
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+/**
+ * Tabs
+ */
+function changeTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
